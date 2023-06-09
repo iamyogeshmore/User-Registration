ad1887bd91d04d2d49eb8b73de0d16a5e1ebd05b
Yogesh More <yogesh.more2098@gmail.com>
2023-06-10 00:21:09 +0530

    Added:UserRegistration class with validateFirstName method.

--- src/main/java/org/bridgelabz/UserRegistration.java
+++ src/main/java/org/bridgelabz/UserRegistration.java
@@ -1,7 +1,25 @@
 package org.bridgelabz;
 
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    public static void main(String[] args) {
-        System.out.println("Welcome to User Registration Program!!!");
+    static Scanner sc = new Scanner(System.in);
+
+    public static void validateFirstName() {
+
+        System.out.print("Enter first name: ");
+        String fName = sc.next();
+        String regex = "^[A-Z]{1}[a-z]*";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(fName);
+        boolean r = matcher.matches();
+
+        if (r)
+            System.out.println("It is a valid first name.");
+        else
+            System.out.println("It is invalid first name.");
     }
 }
---
+++ src/main/java/org/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,27 @@
+package org.bridgelabz;
+
+import java.util.Scanner;
+
+public class UserRegistrationMain {
+    public static void main(String[] args) {
+        System.out.println("************ Welcome to User Registration!!! ************");
+        Scanner input = new Scanner(System.in);
+        int choice;
+        do {
+            System.out.println();
+            System.out.println("1: Validate name.");
+            System.out.println("0: Exit.");
+            System.out.print("Enter your choice: ");
+            choice = input.nextInt();
+
+            switch (choice) {
+                case 1:
+                    UserRegistration.validateFirstName();
+                    break;
+
+                default:
+                    System.out.println("Enter valid input.");
+            }
+        } while (choice != 0);
+    }
+}
